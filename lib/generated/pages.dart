3b0349debc64cb109ab5a6d1a044aed921b370af
FLT <FLT@AKs-MacBook-Pro.local>
2019-08-07 10:39:20 +0100

    Initial commit

---
+++ lib/generated/pages.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ExplorePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
+
+class PastTripPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+    );
+  }
+}
